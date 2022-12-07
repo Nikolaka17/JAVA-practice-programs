dddc0454af0d54f2928c09de3d39c68e0d069ae0
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-06 20:28:27 -0600

    Initial commit

---
+++ PolygonDriver.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class PolygonDriver {
+    public static void main(String[] args){
+        ArrayList<RegularPolygon> polygons = new ArrayList<RegularPolygon>();
+        
+    }
+}
---
+++ RegularPolygon.java
@@ -0,0 +1,6 @@
+public class RegularPolygon {
+    private int numSides;
+    private int length;
+    private int x;
+    private int y;
+}
