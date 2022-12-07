d40d443ebd7725343e25d4ad71496631b91460b1
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-06 20:52:42 -0600

    Add mutator tests

--- PolygonDriver.java
+++ PolygonDriver.java
@@ -13,5 +13,15 @@ public class PolygonDriver {
         for(int i = 0; i < 7; i++){
             polygons.add(new RegularPolygon(rng.nextInt(5)+3, rng.nextInt(20)+1, rng.nextInt(20)-10, rng.nextInt(20)-10));
         }
+
+        //polygons.get(0).setNumSides(1);
+        //polygons.get(0).setLength(-4);
+        for(int i = 0; i < polygons.size(); i++){
+            polygons.get(i).setNumSides(rng.nextInt(5)+3);
+            polygons.get(i).setLength(rng.nextInt(20)+1);
+            polygons.get(i).setX(rng.nextInt(20)-10);
+            polygons.get(i).setY(rng.nextInt(20)-10);
+            System.out.println("P"+(i+1)+" has been mutated");
+        }
     }
 }
