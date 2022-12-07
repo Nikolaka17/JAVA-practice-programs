1bfa871787e5dc79f6303f31b55f1702569f4d57
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-06 20:44:03 -0600

    Add constructor tests

--- PolygonDriver.java
+++ PolygonDriver.java
@@ -1,8 +1,17 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class PolygonDriver {
     public static void main(String[] args){
+        Random rng = new Random();
         ArrayList<RegularPolygon> polygons = new ArrayList<RegularPolygon>();
-        
+        polygons.add(new RegularPolygon());
+        //polygons.add(new RegularPolygon(2, 3));
+        //polygons.add(new RegularPolygon(6, 0));
+        polygons.add(new RegularPolygon(5, 3));
+        polygons.add(new RegularPolygon(4, 4, 3, -1));
+        for(int i = 0; i < 7; i++){
+            polygons.add(new RegularPolygon(rng.nextInt(5)+3, rng.nextInt(20)+1, rng.nextInt(20)-10, rng.nextInt(20)-10));
+        }
     }
 }
