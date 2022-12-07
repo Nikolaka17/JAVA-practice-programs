1b53156f833317456dd7c359e4e01e4696250b5c
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-06 20:53:14 -0600

    Add mutators

--- RegularPolygon.java
+++ RegularPolygon.java
@@ -24,4 +24,26 @@ public class RegularPolygon {
         this.x = x;
         this.y = y;
     }
+
+    public void setNumSides(int n)throws IllegalArgumentException{
+        if(n < 3){
+            throw new IllegalArgumentException("There must be at least 3 sides");
+        }
+        numSides = n;
+    }
+
+    public void setLength(int l)throws IllegalArgumentException{
+        if(!(l > 0)){
+            throw new IllegalArgumentException("Side length must be greater than 0");
+        }
+        length = l;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
 }
