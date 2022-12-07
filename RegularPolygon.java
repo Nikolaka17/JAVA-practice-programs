ebad8c7d3b5f1bb5aac887494d7fa40640df4074
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-06 20:43:33 -0600

    Add constructors

--- RegularPolygon.java
+++ RegularPolygon.java
@@ -3,4 +3,25 @@ public class RegularPolygon {
     private int length;
     private int x;
     private int y;
+
+    public RegularPolygon(){
+        this(3, 1, 0, 0);
+    }
+
+    public RegularPolygon(int n, int l)throws IllegalArgumentException{
+        this(n, l, 0, 0);
+    }
+
+    public RegularPolygon(int n, int l, int x, int y)throws IllegalArgumentException{
+        if(n < 3){
+            throw new IllegalArgumentException("There must be at least 3 sides");
+        }
+        if(!(l > 0)){
+            throw new IllegalArgumentException("Side length must be greater than 0");
+        }
+        numSides = n;
+        length = l;
+        this.x = x;
+        this.y = y;
+    }
 }
