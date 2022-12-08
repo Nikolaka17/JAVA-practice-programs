7da6da541cb9e491af980609a28e9a08f3a226e7
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-08 09:08:02 -0600

    Add functionality

--- Node.java
+++ Node.java
@@ -1,3 +1,13 @@
 public class Node{
+    private String data;
+    public Node left;
+    public Node right;
 
+    public Node(String s){
+        data = s;
+    }
+
+    public String getData(){
+        return data;
+    }
 }
