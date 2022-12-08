e53f78f75850fdbc5519597e6e4d6ff8fd9d99d9
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-08 09:28:56 -0600

    Create insert function

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,42 @@
 public class BinaryTree{
-    
+    private Node root = null;
+
+    public void insert(String newData){
+        Node newNode = new Node(newData);
+        if(root == null){
+            root = newNode;
+        }else{
+            Node parent = null;
+            Node current = newNode;
+            boolean searching = true;
+            do{
+                parent = current;
+                if(newNode.compareTo(current) <= 0){
+                    current = current.left;
+                    if(current == null){
+                        parent.left = newNode;
+                        searching = false;
+                    }
+                }else{
+                    current = current.right;
+                    if(current == null){
+                        parent.right = newNode;
+                        searching = false;
+                    }
+                }
+            }while(searching);
+        }
+    }
+
+    public Node search(){
+        Node current = root;
+    }
+
+    public Node min(){
+
+    }
+
+    public Node max(){
+
+    }
 }
