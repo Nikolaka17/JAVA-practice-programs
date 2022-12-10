8760f5a7824683fe504b8007519aca1556110e78
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-10 13:46:22 -0600

    Add items to window

--- TreeWindow.java
+++ TreeWindow.java
@@ -1,7 +1,8 @@
 import javax.swing.JFrame;
-import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
 
 public class TreeWindow{
 	
@@ -13,10 +14,31 @@ public class TreeWindow{
 		JButton minButton = new JButton("Find min");
 		JButton maxButton = new JButton("Find max");
 		JButton inorderButton = new JButton("Inorder traversal");
-		JButton preorderTraversal = new JButton("Preorder traversal");
-		JButton postorderTraversal = new JButton("Postorder traversal");
+		JButton preorderButton = new JButton("Preorder traversal");
+		JButton postorderButton = new JButton("Postorder traversal");
 		
 		JTextField addField = new JTextField(5);
 		JTextField searchField = new JTextField(5);
+
+        BinaryTree tree = new BinaryTree();
+
+        window.setLayout(new GridLayout(7,2, 5, 20));
+        window.add(addField);
+        window.add(addButton);
+        window.add(searchField);
+        window.add(searchButton);
+        window.add(new JLabel());
+        window.add(minButton);
+        window.add(new JLabel());
+        window.add(maxButton);
+        window.add(new JLabel());
+        window.add(inorderButton);
+        window.add(new JLabel());
+        window.add(preorderButton);
+        window.add(new JLabel());
+        window.add(postorderButton);
+
+        window.setSize(500, 300);
+        window.setVisible(true);
 	}
 }
