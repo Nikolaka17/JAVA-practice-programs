5786c21cb6d20224b7962e92a1b59ef6b8bc75b7
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-10 20:17:51 -0600

    Add action listeners to buttons

--- TreeWindow.java
+++ TreeWindow.java
@@ -2,7 +2,10 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TreeWindow{
 	
@@ -22,6 +25,82 @@ public class TreeWindow{
 
         BinaryTree tree = new BinaryTree();
 
+        addButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+                if(addField.getText() != null && !addField.getText().equals("")){
+                    tree.insert(addField.getText());
+                    JOptionPane.showMessageDialog(window, addField.getText() + " has been added to the tree", "Item added", JOptionPane.INFORMATION_MESSAGE);
+                }else{
+                    JOptionPane.showMessageDialog(window, "No value given", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+                addField.setText("");
+            }
+        });
+        searchButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+                if(searchField.getText() != null && !searchField.getText().equals("")){
+                    if(tree.in(new Node(searchField.getText()))){
+                        JOptionPane.showMessageDialog(window, searchField.getText() + " was found in the tree", "Item found", JOptionPane.INFORMATION_MESSAGE);
+                    }else{
+                        JOptionPane.showMessageDialog(window, searchField.getText() + " was not found in the tree", "Item not found", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                }else{
+                    JOptionPane.showMessageDialog(window, "No value given", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+                searchField.setText("");
+            }
+        });
+        minButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+                JOptionPane.showMessageDialog(window, tree.min() + " has the lowest value of the tree", "Min", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        maxButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+                JOptionPane.showMessageDialog(window, tree.max() + " has the highest value of the tree", "Max", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        inorderButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+                StringBuilder result = new StringBuilder();
+                Node[] nodes = tree.inorder();
+                for(Node n: nodes){
+                    result = result.append(n.getData());
+                    result = result.append("\n");
+                }
+                JOptionPane.showMessageDialog(window, result, "Inorder", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        preorderButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+                StringBuilder result = new StringBuilder();
+                Node[] nodes = tree.preorder();
+                for(Node n: nodes){
+                    result = result.append(n.getData());
+                    result = result.append("\n");
+                }
+                JOptionPane.showMessageDialog(window, result, "Preorder", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+       postorderButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+                StringBuilder result = new StringBuilder();
+                Node[] nodes = tree.postorder();
+                for(Node n: nodes){
+                    result = result.append(n.getData());
+                    result = result.append("\n");
+                }
+                JOptionPane.showMessageDialog(window, result, "Postorder", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
         window.setLayout(new GridLayout(7,2, 5, 20));
         window.add(addField);
         window.add(addButton);
@@ -39,6 +118,8 @@ public class TreeWindow{
         window.add(postorderButton);
 
         window.setSize(500, 300);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
         window.setVisible(true);
 	}
 }
