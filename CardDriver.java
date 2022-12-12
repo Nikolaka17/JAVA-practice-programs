11862bfd9f3a9b0b244c6eb0727de332b7ae706b
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-12 13:24:30 -0600

    Add constructor tests

--- CardDriver.java
+++ CardDriver.java
@@ -1,6 +1,13 @@
+import javax.swing.ImageIcon;
+import java.util.ArrayList;
+
 public class CardDriver {
     
     public static void main(String[] args){
-        
+        ArrayList<Card> cards = new ArrayList<Card>();
+        cards.add(new Card());
+        //cards.add(new Card(Card.Suit.DIAMOND, "1"));
+        cards.add(new Card(Card.Suit.SPADE, "A"));
+        cards.add(new Card(Card.Suit.CLUB, "3", new ImageIcon(), new ImageIcon()));
     }
 }
