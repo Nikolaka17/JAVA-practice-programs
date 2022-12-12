0438191d3c70896aff9368174717dc1c3be21cfc
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-12 13:24:04 -0600

    Add constructors

--- Card.java
+++ Card.java
@@ -1,3 +1,46 @@
+import javax.swing.ImageIcon;
+
 public class Card {
+    public static enum Suit{
+        HEART,
+        CLUB,
+        SPADE,
+        DIAMOND,
+        NONE
+    }
+
+    private Suit suit;
+    private String rank;
+    private ImageIcon back;
+    private ImageIcon front;
+
+    private static final String[] ALLOWED_RANKS = new String[]{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "Joker"};
     
+    public Card(){
+        this(Suit.HEART, "A", null, null);
+    }
+
+    public Card(Suit s, String r)throws IllegalArgumentException{
+        this(s, r, null, null);
+    }
+
+    public Card(Suit s, String r, ImageIcon b, ImageIcon f)throws IllegalArgumentException{
+        if(isValidRank(r)){
+            throw new IllegalArgumentException("Invalid rank. Valid ranks are 2-10, J, Q, K, A, Joker");
+        }
+        suit = s;
+        rank = r;
+        back = b;
+        front = f;
+    }
+
+    private boolean isValidRank(String r){
+        for(String s: ALLOWED_RANKS){
+            if(s.equals(r)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
