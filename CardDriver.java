a4612da50e61f46754c28cfb5feca0fb15ba2be4
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-12 13:35:59 -0600

    Add acessor tests

--- CardDriver.java
+++ CardDriver.java
@@ -9,5 +9,12 @@ public class CardDriver {
         //cards.add(new Card(Card.Suit.DIAMOND, "1"));
         cards.add(new Card(Card.Suit.SPADE, "A"));
         cards.add(new Card(Card.Suit.CLUB, "3", new ImageIcon(), new ImageIcon()));
+
+        for(int i = 0; i < cards.size(); i++){
+            System.out.println("C"+(i+1)+" has a suit of: " + cards.get(i).getSuit());
+            System.out.println("C"+(i+1)+" has a rank of: " + cards.get(i).getRank());
+            System.out.println("C"+(i+1)+" has a back of: " + cards.get(i).getBack());
+            System.out.println("C"+(i+1)+" has a front of: "+ cards.get(i).getFront());
+        }
     }
 }
