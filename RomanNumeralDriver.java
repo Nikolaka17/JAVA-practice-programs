8b8a99e47ee1c6dcbe2e4569da196d443ea081da
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-15 18:48:31 -0600

    Add tests for constructors

--- RomanNumeralDriver.java
+++ RomanNumeralDriver.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class RomanNumeralDriver {
     
     public static void main(String[] args){
-        
+        ArrayList<RomanNumeral> numerals = new ArrayList<RomanNumeral>();
+        numerals.add(new RomanNumeral());
+        numerals.add(new RomanNumeral(632));
+        //numerals.add(new RomanNumeral(0));
+        //numerals.add(new RomanNumeral(10000));
+        //numerals.add(new RomanNumeral("IIII"));
+        numerals.add(new RomanNumeral("LXVI"));
+        numerals.add(new RomanNumeral("CDIX"));
+        numerals.add(RomanNumeral.MIN_NUMERAL);
+        numerals.add(RomanNumeral.MAX_NUMERAL);
     }
 }
