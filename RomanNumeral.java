8c5da52785d2caecb86872903da80a0222d9a5e0
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-15 19:52:30 -0600

    Add javadoc comments

--- RomanNumeral.java
+++ RomanNumeral.java
@@ -1,12 +1,29 @@
+/**
+ * Class that is able to store a roman numeral
+ */
 public class RomanNumeral implements Comparable{
     private int value;
+    /**
+     * A roman numeral with the value of 1. The lowest value storable in a roman numeral
+     */
     public final static RomanNumeral MIN_NUMERAL = new RomanNumeral();
+    /**
+     * A roman numeral with the value of 4000. The highest value storable in a roman numeral
+     */
     public final static RomanNumeral MAX_NUMERAL = new RomanNumeral(4000);
 
+    /**
+     * Creates a roman numeral with a value of 1
+     */
     public RomanNumeral(){
         this(1);
     }
 
+    /**
+     * Creates a roman numeral with a user defined value
+     * @param v The integer value to create a roman numeral of
+     * @throws IllegalArgumentException If the given value is less than 1 or greater than 4000
+     */
     public RomanNumeral(int v)throws IllegalArgumentException{
         if(v < 1 || v > 4000){
             throw new IllegalArgumentException("Can only store values between 1 and 4000");
@@ -14,10 +31,21 @@ public class RomanNumeral implements Comparable{
         value = v;
     }
 
+    /**
+     * Creates a roman numeral with a user defined value
+     * @param v The string representation of the value to store in the roman numeral
+     * @throws NumberFormatException Thrown if the value given isn't a properly formatted roman numeral
+     * @throws IllegalArgumentException Thrown if string represents a value less than 1 or greater than 4000
+     */
     public RomanNumeral(String v)throws NumberFormatException, IllegalArgumentException{
         value = toInt(v); 
     }
 
+    /**
+     * Mutator for the value
+     * @param v The integer value to change to
+     * @throws IllegalArgumentException If the given value is less than 1 or greater than 4000
+     */
     public void setValue(int v)throws IllegalArgumentException{
         if(v < 1 || v > 4000){
             throw new IllegalArgumentException("Can only store values between 1 and 4000");
@@ -25,14 +53,30 @@ public class RomanNumeral implements Comparable{
         value = v;
     }
 
+    /**
+     * Mutator for the value
+     * @param v The string representation of a value to change to
+     * @throws NumberFormatException Thrown if the value given isn't a properly formatted roman numeral
+     * @throws IllegalArgumentException Thrown if string represents a value less than 1 or greater than 4000
+     */
     public void setValue(String v)throws NumberFormatException, IllegalArgumentException{
         value = toInt(v);
     }
 
+    /**
+     * Accessor for the value
+     * @return The value stored by the roman numeral
+     */
     public int getValue(){
         return value;
     }
 
+    /**
+     * Adds two roman numerals
+     * @param n The roman numeral to add with
+     * @return A roman numeral representing the sum of the two roman numerals
+     * @throws ArithmeticException Thrown if the resulting value is greater than 4000
+     */
     public RomanNumeral add(RomanNumeral n)throws ArithmeticException{
         if(value + n.value > 4000){
             throw new ArithmeticException("Result is larger than what can be stored");
@@ -40,6 +84,12 @@ public class RomanNumeral implements Comparable{
         return new RomanNumeral(value + n.value);
     }
 
+    /**
+     * Subtracts two roman numerals
+     * @param n The roman numeral to subtract with
+     * @return A roman numeral representing the difference of the two roman numerals
+     * @throws ArithmeticException Thrown if the resulting value is less than 1
+     */
     public RomanNumeral subtract(RomanNumeral n)throws ArithmeticException{
         if(value - n.value < 1){
             throw new ArithmeticException("Result is smaller than what can be stored");
@@ -47,6 +97,12 @@ public class RomanNumeral implements Comparable{
         return new RomanNumeral(value + n.value);
     }
 
+    /**
+     * Multiplies two roman numerals
+     * @param n The roman numeral to multiply with
+     * @return A roman numeral representing the product of the two roman numerals
+     * @throws ArithmeticException Thrown if the resulting value is greater than 4000
+     */
     public RomanNumeral multiply(RomanNumeral n)throws ArithmeticException{
         if(value * n.value > 4000){
             throw new ArithmeticException("Result is larger than what can be stored");
@@ -54,10 +110,21 @@ public class RomanNumeral implements Comparable{
         return new RomanNumeral(value * n.value);
     }
 
+    /**
+     * Divides two roman numerals
+     * @param n The roman numeral to divide with
+     * @return A roman numeral representing the quotient of the two roman numerals
+     */
     public RomanNumeral divide(RomanNumeral n){
         return new RomanNumeral(value / n.value);
     }
 
+    /**
+     * Raises a roman numeral to a power
+     * @param n The power to raise to
+     * @return A roman numeral representing the roman numeral raised to a power
+     * @throws ArithmeticException Thrown if the resulting value is greater than 4000
+     */
     public RomanNumeral pow(int n)throws ArithmeticException{
         if(Math.pow(value, n) > 4000){
             throw new ArithmeticException("Result is larger than what can be stored");
@@ -65,10 +132,22 @@ public class RomanNumeral implements Comparable{
         return new RomanNumeral((int) Math.pow(value, n));
     }
 
+    /**
+     * Takes a roman numeral mod a number
+     * @param n The number to mod to
+     * @return The value of the roman numeral mod a number
+     */
     public int mod(int n){
         return value % n;
     }
 
+    /**
+     * Converts a roman numeral string to an integer
+     * @param s The string to convert
+     * @return The integer value of the string
+     * @throws NumberFormatException Thrown if the given string isn't a properly formatted roman numeral
+     * @throws IllegalArgumentException Thrown if the strings value is less than 1 or greater than 4000
+     */
     private int toInt(String s)throws NumberFormatException, IllegalArgumentException{
         int total = 0;
         for(int i = s.length() - 1; i >= 0; i--){
@@ -91,6 +170,12 @@ public class RomanNumeral implements Comparable{
         return total;
     }
 
+    /**
+     * Converts a character to it's numerical counterpart
+     * @param c The char to convert
+     * @return The numerical representation of the character
+     * @throws NumberFormatException Thrown if the character isn't used in roman numerals
+     */
     private int valueOf(char c)throws NumberFormatException{
         switch(Character.toUpperCase(c)){
             case 'I':
@@ -112,6 +197,10 @@ public class RomanNumeral implements Comparable{
         }
     }
 
+    /**
+     * Overriden toString method, converts the value of the numeral to a string representation
+     * @return A string representation of the roman numeral
+     */
     @Override
     public String toString(){
         StringBuilder result = new StringBuilder();
@@ -161,11 +250,21 @@ public class RomanNumeral implements Comparable{
         return result.toString();
     }
 
+    /**
+     * Overriden equals method, tests if the values of two roman numerals are equal
+     * @param o The roman numeral to compare to
+     * @return A boolean representing if the values are equal
+     */
     @Override
     public boolean equals(Object o){
         return value == ((RomanNumeral) o).value;
     }
 
+    /**
+     * Overriden compareTo method, compares the value of two roman numerals
+     * @param o The roman numeral to compare to
+     * @return -1 if value is less than given numeral's, 0 if value is equal to given numeral's, 1 if value is greater than given numeral's
+     */
     @Override
     public int compareTo(Object o){
         if(value < ((RomanNumeral) o).value){
