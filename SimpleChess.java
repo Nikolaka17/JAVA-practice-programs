c4bd4455507463429ce4e389ecbad18b21c931a6
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-19 20:10:48 -0600

    Define variables

--- SimpleChess.java
+++ SimpleChess.java
@@ -1,3 +1,14 @@
 public class SimpleChess {
-    
+    public static final char KING = 'K';
+    public static final char QUEEN = 'Q';
+    public static final char KNIGHT = 'N';
+    public static final char BISHOP = 'B';
+    public static final char ROOK = 'R';
+    public static final char PAWN = 'P';
+    public static final char SPACE = ' ';
+
+    public static void main(String[] args){
+        char[][] board = new char[][]{{ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK},{PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN},{SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE},{SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE},{SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE},{SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE},{PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN},{ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK}};
+        int player = 1;
+    }
 }
