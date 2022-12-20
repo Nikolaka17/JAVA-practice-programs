77cdc3efccb25dda55d009870164e31ea7b8d533
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-12-19 20:23:08 -0600

    Add print function

--- SimpleChess.java
+++ SimpleChess.java
@@ -10,5 +10,24 @@ public class SimpleChess {
     public static void main(String[] args){
         char[][] board = new char[][]{{ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK},{PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN},{SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE},{SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE},{SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE},{SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE},{PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN},{ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK}};
         int player = 1;
+        boolean check = false;
+    }
+
+    public void printBoard(char[][] b, int p){
+        if(p == 1){
+            for(char[] a: b){
+                for(char c: a){
+                    System.out.print(c);
+                }
+                System.out.println();
+            }
+        }else{
+            for(int i = 7; i <= 0; i--){
+                for(int j = 0; j < 8; j++){
+                    System.out.print(b[i][j]);
+                }
+                System.out.println();
+            }
+        }
     }
 }
