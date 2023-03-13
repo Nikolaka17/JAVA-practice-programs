17acc5e025394452a65aafe8e1a73c072b6a4680
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-03-13 18:45:30 -0500

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
  * Class that represents a full binary tree
  * @see Node
  */
-public class BinaryTree{
-    private Node root = null;
+public class BinaryTree<T>{
+    private Node<T> root = null;
 
     /**
      * Method to add an item to the tree
      * @param value The value to add the the tree
      */
-    public void add(String value){
+    public void add(T value){
         root = recursiveAdd(root, value);
     }
 
@@ -21,13 +21,13 @@ public class BinaryTree{
      * @param value The value to add
      * @return A node with value added
      */
-    private Node recursiveAdd(Node current, String value){
+    private Node<T> recursiveAdd(Node<T> current, T value){
         if(current == null){
-            return new Node(value);
+            return new Node<T>(value);
         }
-        if((new Node(value)).compareTo(current) < 0){
+        if((new Node<T>(value)).compareTo(current) < 0){
             current.left = recursiveAdd(current.left, value);
-        }else if((new Node(value)).compareTo(current) > 0){
+        }else if((new Node<T>(value)).compareTo(current) > 0){
             current.right = recursiveAdd(current.right, value);
         }else{
             return current;
@@ -41,11 +41,11 @@ public class BinaryTree{
      * @param n The node to check
      * @return A boolean representing if the node is in the binary tree
      */
-    public boolean in(Node n){
+    public boolean in(Node<T> n){
         if(root == null){
             return false;
         }
-        Node current = root;
+        Node<T> current = root;
         while(!current.equals(n)){
             if(n.compareTo(current) <= 0){
                 current = current.left;
@@ -63,11 +63,11 @@ public class BinaryTree{
      * A method that finds the lowest value node in the tree
      * @return The node that has the lowest value
      */
-    public Node min(){
+    public Node<T> min(){
         if(root == null){
             return null;
         }
-        Node current = root;
+        Node<T> current = root;
         while(current.left != null){
             current = current.left;
         }
@@ -78,11 +78,11 @@ public class BinaryTree{
      * A method that finds the highest value node in the tree
      * @return The node that has the highest value
      */
-    public Node max(){
+    public Node<T> max(){
         if(root == null){
             return null;
         }
-        Node current = root;
+        Node<T> current = root;
         while(current.right != null){
             current = current.right;
         }
@@ -93,7 +93,7 @@ public class BinaryTree{
      * A method that preordered traverses the tree
      * @return The array of all the data in the tree preordered
      */
-    public Node[] preorder(){
+    public Node<T>[] preorder(){
         return preorder(root);
     }
 
@@ -102,34 +102,31 @@ public class BinaryTree{
      * @param n The subtree to return the data of
      * @return The array of all the data in the sub-tree
      */
-    public static Node[] preorder(Node n){
+    public Node<T>[] preorder(Node<T> n){
         if(n == null){
             return null;
         }
-        ArrayList<Node> result = new ArrayList<Node>();
+        ArrayList<Node<T>> result = new ArrayList<Node<T>>();
         result.add(n);
         if(n.left != null){
-            for(Node i: preorder(n.left)){
+            for(Node<T> i: preorder(n.left)){
                 result.add(i);
             }
         }
         if(n.right != null){
-            for(Node i: preorder(n.right)){
+            for(Node<T> i: preorder(n.right)){
                 result.add(i);
             }
         }
-        Node[] end = new Node[result.size()];
-        for(int i = 0; i < result.size(); i++){
-            end[i] = result.get(i);
-        }
-        return end;
+        
+        return (Node<T>[]) result.toArray();
     }
 
     /**
      * A method that traverses the tree in order
      * @return The array of all the data in the tree
      */
-    public Node[] inorder(){
+    public Node<T>[] inorder(){
         return inorder(root);
     }
 
@@ -138,34 +135,31 @@ public class BinaryTree{
      * @param n The sub-tree to traverse
      * @return The array of all the data in the subtree
      */
-    public static Node[] inorder(Node n){
+    public Node<T>[] inorder(Node<T> n){
         if(n == null){
             return null;
         }
-        ArrayList<Node> result = new ArrayList<Node>();
+        ArrayList<Node<T>> result = new ArrayList<Node<T>>();
         if(n.left != null){
-            for(Node i: preorder(n.left)){
+            for(Node<T> i: preorder(n.left)){
                 result.add(i);
             }
         }
         result.add(n);
         if(n.right != null){
-            for(Node i: preorder(n.right)){
+            for(Node<T> i: preorder(n.right)){
                 result.add(i);
             }
         }
-        Node[] end = new Node[result.size()];
-        for(int i = 0; i < result.size(); i++){
-            end[i] = result.get(i);
-        }
-        return end;
+        
+        return (Node<T>[]) result.toArray();
     }
 
     /**
      * A method that postorder traverses the tree
      * @return The array of all the data in the tree
      */
-    public Node[] postorder(){
+    public Node<T>[] postorder(){
         return postorder(root);
     }
 
@@ -174,26 +168,23 @@ public class BinaryTree{
      * @param n The subtree to traverse
      * @return The array of all the data in the subtree
      */
-    public static Node[] postorder(Node n){
+    public Node<T>[] postorder(Node<T> n){
         if(n == null){
             return null;
         }
-        ArrayList<Node> result = new ArrayList<Node>();
+        ArrayList<Node<T>> result = new ArrayList<Node<T>>();
         if(n.left != null){
-            for(Node i: preorder(n.left)){
+            for(Node<T> i: preorder(n.left)){
                 result.add(i);
             }
         }
         if(n.right != null){
-            for(Node i: preorder(n.right)){
+            for(Node<T> i: preorder(n.right)){
                 result.add(i);
             }
         }
         result.add(n);
-        Node[] end = new Node[result.size()];
-        for(int i = 0; i < result.size(); i++){
-            end[i] = result.get(i);
-        }
-        return end;
+       
+        return (Node<T>[]) result.toArray();
     }
 }
